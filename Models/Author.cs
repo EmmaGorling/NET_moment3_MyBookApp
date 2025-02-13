88e7bf1e295b0a19beed3e8246d700c158338aed
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2025-02-13 15:00:30 +0100

    Har nu funktionalitet för godkänt, justerat så länkar med mera är på svenska
    Fixa egen styling, eventuellt lägga till tabeller för att kuna låna böcker

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MyBookApp.Models; 
 public class Author {
+    // Properties
     public int Id { get; set; }
+    [Required]
+    [Display(Name = "Namn")]
     public string?  Name { get; set; }
-
+    
+    [Display(Name = "Böcker")]
     public List<Book>? Books { get; set; }
 }
--- Models/Book.cs
+++ Models/Book.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MyBookApp.Models;
 
 public class Book {
+    // Properties
     public int Id { get; set; }
+
+    [Required]
+    [Display(Name = "Titel")]
     public string? Title { get; set; }
-    public string Genre { get; set; }
+
+    [Required]
+    public string? Genre { get; set; }
 
     public int AuthorId { get; set; }
+
+    [Display(Name = "Författare")]
     public Author? Author { get; set; }
 }
